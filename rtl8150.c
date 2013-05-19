f53e2e6247c7b949097d4104caff8fd6bd6d405f
Petko Manolov <petkan@nucleusys.com>
2013-05-19 13:02:48 +0300

    make structure assignments pretier;

--- rtl8150.c
+++ rtl8150.c
@@ -683,15 +683,15 @@ static int rtl8150_ioctl(struct net_device *netdev, struct ifreq *rq, int cmd)
 }
 
 static const struct net_device_ops rtl8150_netdev_ops = {
-	.ndo_open = rtl8150_open,
-	.ndo_stop = rtl8150_close,
-	.ndo_do_ioctl = rtl8150_ioctl,
-	.ndo_start_xmit = rtl8150_start_xmit,
-	.ndo_tx_timeout = rtl8150_tx_timeout,
-	.ndo_set_rx_mode = rtl8150_set_multicast,
-	.ndo_set_mac_address = rtl8150_set_mac_address,
-	.ndo_change_mtu	= eth_change_mtu,
-	.ndo_validate_addr = eth_validate_addr,
+	.ndo_open		= rtl8150_open,
+	.ndo_stop		= rtl8150_close,
+	.ndo_do_ioctl		= rtl8150_ioctl,
+	.ndo_start_xmit		= rtl8150_start_xmit,
+	.ndo_tx_timeout		= rtl8150_tx_timeout,
+	.ndo_set_rx_mode	= rtl8150_set_multicast,
+	.ndo_set_mac_address	= rtl8150_set_mac_address,
+	.ndo_change_mtu		= eth_change_mtu,
+	.ndo_validate_addr	= eth_validate_addr,
 };
 
 static int rtl8150_probe(struct usb_interface *intf,
@@ -772,12 +772,12 @@ static void rtl8150_disconnect(struct usb_interface *intf)
 }
 
 static struct usb_driver rtl8150_driver = {
-	.name = driver_name,
-	.probe = rtl8150_probe,
-	.disconnect = rtl8150_disconnect,
-	.id_table = rtl8150_table,
-	.suspend = rtl8150_suspend,
-	.resume = rtl8150_resume,
+	.name		= driver_name,
+	.probe		= rtl8150_probe,
+	.disconnect	= rtl8150_disconnect,
+	.id_table	= rtl8150_table,
+	.suspend	= rtl8150_suspend,
+	.resume		= rtl8150_resume,
 	.disable_hub_initiated_lpm = 1,
 };
 
